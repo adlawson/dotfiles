5327c49b03db66bc68d110ef84de13177dde88c9
adlawson <adlawson@gmail.com>
2013-08-04 00:17:37 +0100

    Ignore install script during install

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) "
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-    rsync --exclude ".git/" --exclude ".DS_Store" --exclude "install" \
+    rsync --exclude ".git/" --exclude ".DS_Store" --exclude "install.sh" \
           --exclude "README.md" -av --no-perms . ~
     source ~/.bash_profile
 fi
