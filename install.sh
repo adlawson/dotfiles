a48b52bb6805937fdf1cd088bc127261f2d73bfe
adlawson <adlawson@gmail.com>
2013-08-03 23:50:21 +0100

    Add install script

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) "
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    rsync --exclude ".git/" --exclude ".DS_Store" --exclude "install" \
+          --exclude "README.md" -av --no-perms . ~
+    source ~/.bash_profile
+fi
+
