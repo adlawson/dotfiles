c372a83ea0b10ce38d3db68b9bd2b8d553059ad8
adlawson <adlawson@gmail.com>
2013-08-04 19:30:27 +0100

    Ignore .gitmodules on install

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@ read -p "This may overwrite existing files in your home directory. Are you sure?
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
     rsync --exclude ".git/" --exclude ".DS_Store" --exclude "install.sh" \
-          --exclude "README.md" -av --no-perms . ~
+          --exclude "README.md" --exclude ".gitmodules" -av --no-perms . ~
     source ~/.bash_profile
 fi
