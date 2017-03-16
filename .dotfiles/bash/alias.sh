fcd8e07362ee2ba876bc16976990421176c03b77
adlawson <adlawson@gmail.com>
2017-03-16 17:37:26 +0000

    Add sbt bash alias

--- .dotfiles/bash/alias.sh
+++ .dotfiles/bash/alias.sh
@@ -1,1 +1,8 @@
 alias vargant="vagrant"
+
+alias sbt="docker run --rm -it \
+    -v "$HOME/.ivy2":/root/.ivy2 \
+    -v `pwd`:/src \
+    -e ARTIFACTORY_USER \
+    -e ARTIFACTORY_PASS \
+    adlawson/sbt:fakeroot"
