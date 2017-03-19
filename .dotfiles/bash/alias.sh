bc787e97e30b75e20197b540d00f93b1490bed30
adlawson <adlawson@gmail.com>
2017-03-19 15:07:34 +0000

    Fix alias quotes

--- .dotfiles/bash/alias.sh
+++ .dotfiles/bash/alias.sh
@@ -1,18 +1,18 @@
 alias ansible-vault="docker run --rm -i \
-    -v "$PWD":/src \
+    -v \"$PWD\":/src \
     adlawson/ansible-vault:2.0"
 
 alias mysql="docker run --rm -i \
-    -v $HOME/.my.cnf:/etc/mysql/conf.d/my.cnf \
+    -v \"$HOME\"/.my.cnf:/etc/mysql/conf.d/my.cnf \
     adlawson/mysql:5.7 mysql"
 
-alias node="docker run --rm -it -v $PWD:/src -w /src node"
+alias node="docker run --rm -it -v \"$PWD\":/src -w /src node"
 
-alias npm="docker run --rm -it -v $PWD:/src -w /src --entrypoint npm node"
+alias node="docker run --rm -it -v \"$PWD\":/src -w /src --entrypoint npm node"
 
 alias sbt="docker run --rm -it \
-    -v "$HOME/.ivy2":/root/.ivy2 \
-    -v "$PWD":/src \
+    -v \"$HOME/.ivy2\":/root/.ivy2 \
+    -v \"$PWD\":/src \
     -e ARTIFACTORY_USER \
     -e ARTIFACTORY_PASS \
     adlawson/sbt:fakeroot"
