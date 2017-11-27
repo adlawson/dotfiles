b5895a4620dee0eec9d2bba0cdb5aa17a3026a43
adlawson <adlawson@gmail.com>
2017-11-27 10:12:59 +0000

    Update docker aliases

--- .dotfiles/bash/alias.sh
+++ .dotfiles/bash/alias.sh
@@ -1,17 +1,19 @@
-alias ansible-vault='docker run --rm -i -v $PWD:/src --entrypoint ansible-vault adlawson/ansible:2.0'
+alias ansible-vault='docker run --rm -i -v $PWD:/src -v $HOME/.ansiblevault:/root/.ansiblevault --entrypoint ansible-vault adlawson/ansible:2.0'
 
-#alias mysql='docker run --rm -i -v $HOME/.my.cnf:/etc/mysql/conf.d/my.cnf adlawson/mysql:5.7 mysql'
+alias aws='docker run --rm -it -v $PWD:/src -w /src -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -e AWS_DEFAULT_REGION mesosphere/aws-cli'
+
+alias keytool='docker run --rm -it -v $PWD:/src -w /src --entrypoint keytool openjdk'
 
 alias node='docker run --rm -it -v $PWD:/src -w /src node'
 
 alias npm='docker run --rm -it -v $PWD:/src -v $HOME/.npm:/root/.npm -v $HOME/.npmrc:/root/.npmrc -w /src node npm'
 
-alias sbt='docker run --rm -it -v $HOME/.ivy2:/root/.ivy2 -v $PWD:/src -e ARTIFACTORY_USER -e ARTIFACTORY_PASS --entrypoint sbt adlawson/sbt:fakeroot'
+alias sbt='docker run --rm -it -v $HOME/.ivy2:/root/.ivy2 -v $PWD:/src -v /var/run/docker.sock:/var/run/docker.sock -e ARTIFACTORY_USER -e ARTIFACTORY_PASS --entrypoint sbt adlawson/sbt'
 
-alias scala='docker run --rm -it -v $PWD:/src --entrypoint scala adlawson/scala:2.12'
+alias scala='docker run --rm -it -v $PWD:/src --entrypoint scala adlawson/scala'
 
-alias scalac='docker run --rm -it -v $PWD:/src --entrypoint scalac adlawson/scala:2.12'
+alias scalac='docker run --rm -it -v $PWD:/src --entrypoint scalac adlawson/scala'
 
-alias vargant='vagrant'
+alias travis='docker run --rm -it -v $HOME/.travis:/root/.travis -v $PWD:/src adlawson/travis'
 
 alias vault='docker run --rm -it -p 8200:8200 vault'
