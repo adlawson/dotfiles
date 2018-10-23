f4d2cc91f6bfa4b88257e1eb1eb222f42f2f139e
adlawson <adlawson@gmail.com>
2018-10-23 10:01:08 +0100

    Update brew install list

--- brew.sh
+++ brew.sh
@@ -2,21 +2,24 @@
 
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+brew install bash
 brew install wget
 brew install git
 brew install vim
 brew install pass
-brew install gnupg2
 brew install bash-completion
+brew install kubectl
+brew install gnupg2
 brew install pinentry-mac
-brew install mysql
+
+brew install go
+brew install nodejs
 
 brew cask install google-chrome
-brew cask install gpg-suite
 brew cask install iterm2
 brew cask install slack
-brew cask install atom
 brew cask install docker
+brew cask install minikube
 brew cask install keybase
 brew cask install spotify
-brew cask install clamxav
+brew cask install vscode
